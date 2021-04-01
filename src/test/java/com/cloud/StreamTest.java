cd45ab78f5c55be1f2560f1d91be2bc252fd7001
yangshuai <2237632828@qq.com>
2021-04-01 14:54:12 +0800

    stream

--- src/test/java/com/cloud/StreamTest.java
+++ src/test/java/com/cloud/StreamTest.java
@@ -15,9 +15,73 @@ import java.util.stream.Collectors;
 @SpringBootTest
 public class StreamTest {
 
+    public void testMap(){
+
+    }
+
+    //maxBy minBy 最大值和最小值
+    @Test
+    public void testSt14(){
+        List<Student> students = testStream();
+        Optional<Student> max = students.stream().collect(Collectors.maxBy(Comparator.comparing(Student::getAge)));
+        Optional<Student> min = students.stream().collect(Collectors.minBy(Comparator.comparing(Student::getAge)));
+        System.out.println(max+"--"+min);
+    }
+    //count 统计总数
+    @Test
+    public void testSt13(){
+        List<Student> students = testStream();
+        long count = students.stream().count();
+        System.out.println(count);
+    }
+    //findAny 找到任意一个元素
+    @Test
+    public void testSt12(){
+        List<Student> students = testStream();
+        Optional<Student> student = students.stream().findAny();
+        System.out.println(student.toString());
+    }
+    //findFirst 找到第一个元素
+    @Test
+    public void testSt11(){
+        List<Student> students = testStream();
+        Optional<Student> first = students.stream().sorted(Comparator.comparingInt(Student::getAge)).findFirst();
+        System.out.println(first.toString());
+    }
+    //noneMatchT 流中是否有元素匹配给定的T
+    @Test
+    public void testSt10(){
+        List<Student> list = testStream();
+        boolean b = list.stream().noneMatch(student -> student.getAddress().contains("郑州"));
+        System.out.println(b);
+    }
+    //anyMatch 检测是否有任意元素满足给定的条件
+    @Test
+    public void testSt9(){
+        List<Student> students = testStream();
+        boolean b = students.stream().anyMatch(student -> student.getSex() == 1);
+        System.out.println(b);
+    }
+    //allMatch 检测是否全部满足参数行为
+    @Test
+    public void testSt8(){
+        List<Student> list = testStream();
+        boolean b = list.stream().allMatch(student -> student.getAge() >= 17);
+        System.out.println(b);
+    }
     //flatMap(T -> Stream<R>)
     @Test
     public void testSt7()  {
+        List<String> flatmap = new ArrayList<>();
+        flatmap.add("常宣灵,常昊灵");
+        flatmap.add("孟婆,判官红,判官蓝");
+         flatmap = flatmap.stream()
+                .map(s -> s.split(","))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+        for (String s : flatmap) {
+            System.out.println(s);
+        }
 
     }
 
